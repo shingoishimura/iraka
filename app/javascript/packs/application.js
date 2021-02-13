2dd9beb70b65a59c671cfbf596ecf0094b918178
shingoishimura <shingoishimuratw@gmail.com>
2021-02-13 18:09:25 +0900

    修正　３

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery") //jQueryを追加
+require("bootstrap") //Bootstrapを追加
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
