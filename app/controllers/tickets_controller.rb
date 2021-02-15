5cc8aa3632d27b56e314fc9f70e1cab67e6d4146
shingoishimura <shingoishimuratw@gmail.com>
2021-02-15 10:00:52 +0900

    公演情報登録機能　土台作成

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,14 +1,30 @@
 class TicketsController < ApplicationController
   def index
+    @tickets = Ticket.all
+  end
+
+  def new
+    @ticket = Ticket.new
   
   end
 
   def create 
-  
+   @ticket = Ticket.new(ticket_params)
+   if @ticket.save
+
+    redirect_to root_path
+  else
+    render :new
+  end
   end
 
   def show
 
   end
 
+  private
+
+  def ticket_params
+    params.reqire(:ticket).permit(:program, :venue, :date, :time, :price,:remaining ).merge(user_id: current_user.id)
+  end
 end
